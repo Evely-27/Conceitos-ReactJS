4896ee8a52a6b51482f9e39d2aef20a31082d9dc
Evely-27 <evelysantos27@gmail.com>
2022-10-03 12:09:11 -0300

    feat: practice with useRef

--- src/App/pages/login/Login.tsx
+++ src/App/pages/login/Login.tsx
@@ -1,22 +1,19 @@
-import { useCallback, useMemo, useState } from "react"
+import { useCallback, useMemo, useRef, useState } from "react"
 
 export const Login = () => {
+// useRef  = guarda algum valor que pode ser alterado sem precisar cirar um novo render, até que  mudemos ele.
+// pega a referencia de um elemento html
+// devemos passar a variavel para o elemento que queremos resgatar de referencia
+const inputPasswordRef = useRef<HTMLInputElement>(null);
+
 
     const [email,setEmail] = useState('');
     const [senha,setSenha] = useState('');
 
-
-
-// ao usar o useMemo = armazenar o calculo na memoria, parecido com o effect,
-// ao mudar o valor de email é que ele vai calcular o novo valor, necessita de dependecia para saber o que mudar.
-// permite calculos sem alterar todos os useState, só o que ele quer.
     const emailLength = useMemo(() => {
         // console.log("Executou!!")
         return email.length * 200;
     }, [email.length]);
-
-
-// useCallback = ele memoriza funções na memoria. Se não houver o array de dependeica não altera a função
 
     const handleEntrar = useCallback(() =>  {
         console.log(email, senha);
@@ -29,12 +26,19 @@ export const Login = () => {
                 <label>
                     <span>EMAIL:</span>
 
-                    <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+                    <input type="text" 
+                    value={email} 
+                    onChange={e => setEmail(e.target.value)}
+                    onKeyDown={e => e.key === 'Enter' ? inputPasswordRef.current?.focus() : undefined}
+                    />
                 </label>
 
                 <label>
                     <span>SENHA:</span>
-                    <input type="password" value={senha } onChange={e => setSenha(e.target.value)} />
+                    <input type="password" 
+                    value={senha } 
+                    ref= {inputPasswordRef}
+                    onChange={e => setSenha(e.target.value)} />
                 </label>
 
                 <button type="button" onClick={handleEntrar}>
